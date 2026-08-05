73eb355c474612b74a31d11b277fd55cfad4e361
Rehnoor Aulakh <aulakhrehnoor@gmail.com>
2026-08-05 08:05:47 +0530

    Sorted filter added to cabins

--- src/ui/Select.jsx
+++ src/ui/Select.jsx
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-
 const StyledSelect = styled.select`
   font-size: 1.4rem;
   padding: 0.8rem 1.2rem;
@@ -13,3 +12,15 @@ const StyledSelect = styled.select`
   font-weight: 500;
   box-shadow: var(--shadow-sm);
 `;
+
+export default function Select({ options, value, onChange, ...props }) {
+  return (
+    <StyledSelect value={value} onChange={onChange} {...props}>
+      {options.map((option) => (
+        <option value={option.value} key={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </StyledSelect>
+  );
+}
