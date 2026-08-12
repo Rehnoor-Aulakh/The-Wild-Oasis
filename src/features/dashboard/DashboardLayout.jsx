1f18706274c627d8f5b8b80a489b25b3e3e41b8b
Rehnoor Aulakh <aulakhrehnoor@gmail.com>
2026-08-12 17:25:37 +0530

    Added RECHARTS library and pie chart for stays and area chart for sales

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -4,6 +4,8 @@ import Spinner from "../../ui/Spinner";
 import useRecentStays from "./useRecentStays";
 import Stats from "./Stats";
 import useCabins from "../cabins/useCabins";
+import SalesChart from "./SalesChart";
+import DurationChart from "./DurationChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -39,8 +41,8 @@ export default function DashboardLayout() {
         cabinCount={Number(cabins?.length)}
       />
       <div>Today&apos;s activities</div>
-      <div>Chart Stay Durations</div>
-      <div>Chart Sales</div>
+      <DurationChart confirmedStays={confirmedStays} />
+      <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
 }
